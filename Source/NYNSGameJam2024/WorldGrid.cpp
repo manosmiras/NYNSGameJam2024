98d9c36c505f06f429717c03a0e187428b926b1a
Manos Miras <manosmiras@outlook.com>
2024-01-08 01:56:59 +0200

    feat: spawn tile grid

---
+++ Source/NYNSGameJam2024/WorldGrid.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldGrid.h"
+
+#include "Engine/StaticMeshActor.h"
+
+void AWorldGrid::SpawnTileGrid()
+{
+	auto World = GetWorld();
+	if(World)
+	{
+		for (int y = 0; y < GridSizeY; ++y)
+		{
+			for (int x = 0; x < GridSizeX; ++x)
+			{
+				FVector Location = FVector(x * TileSize, y * TileSize, 0);
+				FRotator Rotation = FRotator(0, 0, 0);
+				FActorSpawnParameters SpawnInfo;
+				AStaticMeshActor* SpawnedStaticMeshActor = World->SpawnActor<AStaticMeshActor>(
+					AStaticMeshActor::StaticClass(),
+					Location,
+					Rotation,
+					SpawnInfo
+				);
+				if(SpawnedStaticMeshActor)
+				{
+					SpawnedStaticMeshActor->SetMobility(EComponentMobility::Movable);
+					SpawnedStaticMeshActor->GetStaticMeshComponent()->SetStaticMesh(TileMesh);
+					SpawnedStaticMeshActor->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
+				}
+			}
+		}
+	}
+}
+// Called when the game starts or when spawned
+void AWorldGrid::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/NYNSGameJam2024/WorldGrid.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WorldGrid.generated.h"
+
+UCLASS()
+class NYNSGAMEJAM2024_API AWorldGrid : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditInstanceOnly)
+	int GridSizeX;
+	UPROPERTY(EditInstanceOnly)
+	int GridSizeY;
+	UPROPERTY(EditInstanceOnly)
+	int TileSize;
+	UPROPERTY(EditInstanceOnly)
+	UStaticMesh* TileMesh;
+	UFUNCTION(BlueprintCallable)
+	void SpawnTileGrid();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
